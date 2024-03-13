d11bcc37c8ce11dad4539b41c296b52b65b4e155
takker99 <37929109+takker99@users.noreply.github.com>
2024-03-13 10:09:56 +0900

    0.3.0
    [pin-diary-3](https://scrapbox.io/villagepump/pin-diary-3)のコード
    `li.pin`が間違えて生成されるバグを消した

--- main.js
+++ main.js
@@ -1,120 +1,127 @@
 const targetProject = 'villagepump';
-let VERBOSE = false;
-let executed = false;
-let diaryCard = null;
+let updateTimer;
+const handleChange = () => {
+	scrapbox.Layout === "list" && scrapbox.Project.name === targetProject ?
+		startObserve() : endObserve();
+}
 
-export function execute({verbose = false} = {}) {
-  if (executed) return;
-  executed = true;
-  VERBOSE = verbose;
+// initialize
+handleChange();
+scrapbox.addListener("layout:changed", () => handleChange());
+function pinDiary() {
+  if (scrapbox.Layout !== "list") return;
+  if (isValidPostion()) return;
 
+  const diaryCard = getDiaryCard();
+
+  // 一旦pin留めをリセットする
+  removePin(diaryCard);
+
+  const cards = cardList();
+  // Pinしたカードの数
+  // 日付ページにPinする前の数を取得しておく
+  const pins = cards
+    .getElementsByClassName('page-list-item grid-style-item pin')
+    .length;
+
+  // DOMにPin留めをつける
+  appendPin(diaryCard);
+  //日付ページが常にPinなしページの前に来るように移動する
+  cards.insertBefore(diaryCard, cards.children[pins]);
+}
+
+function startObserve() {
+  endObserve();
+  updateTimer = setInterval(pinDiary, 1000);
+}
+function endObserve() {
+  clearInterval(updateTimer);
+}
+function toYYYYMMDD(date) {
+  return `${date.getFullYear()}/${zero(date.getMonth() + 1)}/${zero(date.getDate())}`;
+}
+function zero(n) {
+  return String(n).padStart(2, '0');
+}
+
+function cardList() {
+  return document
+    .getElementsByClassName('page-list')?.[0]
+    ?.getElementsByClassName('grid')?.[0];
+}
+
+/** 日記ページを取得する
+ *
+ * なければ新しく作る
+ */
+function getDiaryCard() {
+  deleteGeneratedCards();
   const today = toYYYYMMDD(new Date());
-  const path = `/${targetProject}/${encodeURIComponent(today)}`
-  handlePageChange(async () => {
-    // top page以外では実行しない
-    if (scrapbox.Project.name !== targetProject && scrapbox.Layout === 'list') return;
+  const path = `/${targetProject}/${encodeURIComponent(today)}`;
+  const cards = cardList();
 
-    // 全てのカードを含む<ul>
-    const cards = document
-      .getElementsByClassName('page-list')?.[0]
-      ?.getElementsByClassName('grid')?.[0];
-
-    // ページの並び替えが終わるまで少し待つ
-    await sleep(1000);
-
-    // 前回の日付ページに相当するカードのPinを外す
-    if(diaryCard) {
-      _log('Removing a pin from %o \n href = %o', diaryCard, diaryCard.querySelector('a').href);
-      removePin({card: diaryCard});
-      _log('Removed.');
-    }
-    // 日付ページを取得する
-    diaryCard = cards
-      .querySelector(`li.page-list-item a[href="${path}"]`)
-      ?.parentNode
-      // なければ新しく作る
-      ?? (() => {
-        _log('The diary card doesn\'t exist. Creating new diary card...');
-        return createEmptyPageCard({title: today, path: path});
-      })();
-    _log({diaryCard});
-
-    // Pinしたカードの数
-    // 日付ページにPinする前の数を取得しておく
-    const pins = cards
-      .getElementsByClassName('page-list-item grid-style-item pin')
-      .length;
-    _log({pins});
-
-    // DOMにPin留めをつける
-    _log('Adding a pin on %o \n href = %o', diaryCard, diaryCard.querySelector('a').href);
-    appendPin({card: diaryCard});
-    _log('Added a pin on the diary card.');
-    //日付ページが常にPinなしページの前に来るように移動する
-    _log('Move the diary card before %o', cards.children[pins]);
-    cards.insertBefore(diaryCard, cards.children[pins]);
-    console.log('Finish pinning the today\'s diary page.')
-  });
+  return cards
+    .querySelector(`li.page-list-item a[href="${path}"]`)
+    ?.parentNode
+    ?? createEmptyPageCard(today, path);
 }
 
-const zero = n => String(n).padStart(2, '0');
-const toYYYYMMDD =
-  date => `${date.getFullYear()}/${zero(date.getMonth() + 1)}/${zero(date.getDate())}`
-const sleep = milliseconds => new Promise(resolve => setTimeout(resolve, milliseconds));
+/** 日記ページが所定の位置にPin留めされているかどうかを調べる
+ *
+ * 条件
+ * - `.pin`がある
+ * - 最初のカードから日記ページのカードまでの間にpinなしカードが存在しない
+ */
+function isValidPostion() {
+  const card = getDiaryCard();
+  if (!card.classList.contains("pin")) return false;
 
-function createEmptyPageCard({title, path}) {
-  const card = document.createElement('li');
-  card.insertAdjacentHTML('beforeend',
-`<li id="pin-diary-card" class="page-list-item grid-style-item" style="opacity: 0.7;">
-  <a href="${path}" rel="route">
-    <div class="hover"></div>
-    <div class="content">
-      <div class="header">
-        <div class="title">${title}</div>
-      </div>
-      <div class="description">
-        <div class="line-img">
-          <div></div><div></div><div></div><div></div><div></div>
-        </div>
-      </div>
-    </div>
-  </a>
-</li>`);
-  return card;
+  const cards = cardList();
+  const length = cards.getElementsByClassName('page-list-item grid-style-item pin').length;
+  for (let i = 0; i < length; i++) {
+    if (!cards.children[i].classList.contains("pin")) return false;
+  }
+  return true;
 }
-function appendPin({card}) {
+
+function appendPin(card) {
   card.getElementsByClassName('hover')?.[0]
     ?.insertAdjacentHTML('afterend', '<div class="pin"></div>');
   card.classList.add('pin');
 }
-function removePin({card}) {
+function removePin(card) {
   card.getElementsByClassName('pin')?.[0]?.remove();
   card.classList.remove('pin');
 }
-function handlePageChange(callback) {
-  callback(); // 初回の実行
-  const observer = new MutationObserver(() => callback());
 
-  // 監視を開始する
-  observer.observe(document.getElementsByTagName('title')[0], {childList: true});
-  observer.observe(document.getElementById('dropdownMenuSort'),
-    // この３つを指定しないと変更を捕捉できない
-    {subtree: true,childList:true,characterData:true});
-};
-function _log(message, ...objects) {
-  if (!VERBOSE) return;
-  const header = '[pin-diary]';
-  if (objects.length !== 0) {
-    if ((typeof message) === 'string') {
-      console.log(`${header}${message}`, ...objects);
-    } else {
-      console.log(header, message, ...objects);
-    }
-  } else {
-    if ((typeof message) === 'string') {
-      console.log(`${header}${message}`);
-    } else {
-      console.log(header, message);
-    }
-  }
+/** このscriptで作成したカードを全部消す */
+function deleteGeneratedCards() {
+  document.querySelectorAll('li[data-id="pin-diary-card"]')
+    .forEach(card => card.remove());
 }
+
+function createEmptyPageCard(title, path) {
+  const card = document.createElement('li');
+  card.dataset.id = "pin-diary-card";
+  card.classList.add("page-list-item", "grid-style-item");
+  card.style.opacity = "0.7";
+  card.insertAdjacentHTML('beforeend',
+`<a href="${path}" rel="route">
+  <div class="hover"></div>
+  <div class="content">
+    <div class="header">
+      <div class="title">${title}</div>
+    </div>
+    <div class="description">
+      <div class="line-img">
+        <div></div><div></div><div></div><div></div><div></div>
+      </div>
+    </div>
+  </div>
+</a>`);
+  return card;
+}
+ scrapbox.on('project:changed', () => {
+ 	//トップページからトップページに移動してもlayout:changedは呼ばれないので、ここでhandleChangeをかける
+ 	handleChange()
+ })
